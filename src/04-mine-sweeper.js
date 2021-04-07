d223a26f0dfedd470c5afbd42414d68245d4f1e7
Artsiom Tratsiuk <inqui05@gmail.com>
2021-04-08 00:29:49 +0300

    feat: solve fourth task

--- src/04-mine-sweeper.js
+++ src/04-mine-sweeper.js
@@ -21,8 +21,28 @@
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new Error('Not implemented');
+function minesweeper(matrix) {
+  const arr = [];
+  matrix.forEach(() => arr.push([]));
+
+  matrix.forEach((elemY, indexY) => elemY.forEach((elemX, indexX) => {
+    let count = 0;
+    const startY = indexY === 0 ? 0 : (indexY - 1);
+    const endY = indexY === (matrix.length - 1) ? indexY : (indexY + 1);
+    const startX = indexX === 0 ? 0 : (indexX - 1);
+    const endX = indexX === (elemY.length - 1) ? indexX : (indexX + 1);
+
+    for (let i = startY; i <= endY; i++) {
+      for (let j = startX; j <= endX; j++) {
+        if (!(i === indexY && j === indexX) && matrix[i][j] === true) {
+          count++;
+        }
+      }
+    }
+    arr[indexY].push(count);
+  }));
+
+  return arr;
 }
 
 module.exports = minesweeper;
