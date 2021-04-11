eed0c86a6b26e387695cd4ca74887fa7dc47d749
Artsiom Tratsiuk <inqui05@gmail.com>
2021-04-12 00:17:45 +0300

    feat: solve tenth task

--- src/10-find-index.js
+++ src/10-find-index.js
@@ -11,8 +11,35 @@
  * For ([1, 2, 3], 2) should return 1
  *
  */
-function findIndex(/* array, value */) {
-  throw new Error('Not implemented');
+function findIndex(array, value) {
+  const newArr = [...array];
+  let divisor = newArr.length;
+  if (newArr.length >= 100) {
+    divisor = 2;
+  }
+  const step = parseInt(newArr.length / divisor, 10);
+  let countStep = 0;
+  let index;
+
+  if (value > newArr[step]) {
+    countStep++;
+    newArr.splice(0, step);
+  } else {
+    newArr.splice(step, newArr.length - step);
+  }
+
+  if (newArr.length > 100) {
+    index = countStep * step + findIndex(newArr, value);
+  } else {
+    let topIndex;
+    newArr.forEach((elem, ind) => {
+      if (elem === value) {
+        topIndex = ind;
+      }
+    });
+    index = countStep * step + topIndex;
+  }
+  return index;
 }
 
 module.exports = findIndex;
