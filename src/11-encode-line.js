c5c07eddab9c48cbb6cdc0d138376e5a92d101c9
Artsiom Tratsiuk <inqui05@mail.com>
2021-04-12 13:10:45 +0300

    feat: solve eleventh task

--- src/11-encode-line.js
+++ src/11-encode-line.js
@@ -8,8 +8,38 @@
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new Error('Not implemented');
+function encodeLine(str) {
+  const arr = str.split('');
+  const finalStr = [];
+  let count = 0;
+  let current = arr[0];
+
+  arr.forEach((elem, index) => {
+    if (current === elem) {
+      count++;
+      if (index === arr.length - 1) {
+        finalStr.push(count);
+        finalStr.push(current);
+      }
+    } else {
+      finalStr.push(count);
+      finalStr.push(current);
+      current = elem;
+      count = 1;
+      if (index === arr.length - 1) {
+        finalStr.push(count);
+        finalStr.push(current);
+      }
+    }
+  });
+
+  finalStr.forEach((elem, index) => {
+    if (elem === 1) {
+      finalStr.splice(index, 1);
+    }
+  });
+
+  return finalStr.join('');
 }
 
 module.exports = encodeLine;
